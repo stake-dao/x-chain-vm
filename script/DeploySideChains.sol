4615842d9921bae6409dc04cfc90045761497b89
Warren <99427649+kobe-eth@users.noreply.github.com>
2023-01-26 13:31:17 +0100

    fix: deployment script

--- script/DeploySideChains.sol
+++ script/DeploySideChains.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.17;
 import "forge-std/Script.sol";
 
 import {Platform} from "src/Platform.sol";
+import {AxelarExecutable} from "src/AxelarExecutable.sol";
 import {CurveGaugeControllerOracle} from "src/CurveGaugeControllerOracle.sol";
 
 contract DeploySideChains is Script {
@@ -11,15 +12,19 @@ contract DeploySideChains is Script {
     address internal constant ETH_STATE_SENDER = 0x0000000000000000000000000000000000000000;
 
     /// Arbitrum Axelar Gateway.
-    address internal constant _ANYCALL = 0x37414a8662bC1D25be3ee51Fb27C2686e2490A89;
+    address internal constant _AXELAR_GATEWAY = 0xe432150cce91c13a887f7D836923d5597adD8E31;
 
     Platform platform;
     CurveGaugeControllerOracle oracle;
+    AxelarExecutable axelarExecutable;
 
     function run() public {
         vm.startBroadcast();
 
-        oracle = new CurveGaugeControllerOracle(_ANYCALL);
+        oracle = new CurveGaugeControllerOracle(address(0));
+        axelarExecutable = new AxelarExecutable(_AXELAR_GATEWAY, ETH_STATE_SENDER, address(oracle));
+        oracle.setAxelarExecutable(address(axelarExecutable));
+
         platform = new Platform(address(oracle));
 
         vm.stopBroadcast();
