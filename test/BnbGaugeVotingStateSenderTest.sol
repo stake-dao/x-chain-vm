8add96568ac6bd6b0278c40fae463e9f3da1623e
PumpkingWok <andrearanma@gmail.com>
2024-08-23 12:18:16 +0200

    feat: add tests in bnbGaugeVotingStateSender

--- test/BnbGaugeVotingStateSenderTest.sol
+++ test/BnbGaugeVotingStateSenderTest.sol
@@ -6,6 +6,7 @@ import {IAxelarGateway} from "src/interfaces/IAxelarGateway.sol";
 import {IAxelarGasReceiverProxy} from "src/interfaces/IAxelarGasReceiverProxy.sol";
 import {BnbGaugeVotingStateSender} from "src/BnbGaugeVotingStateSender.sol";
 import {IPlatform} from "src/interfaces/IPlatform.sol";
+import {IGaugeVoting} from "src/interfaces/IGaugeVoting.sol";
 import {AxelarExecutableClaimer} from "src/AxelarExecutableClaimer.sol";
 import {MockPancakePlatform} from "test/mocks/MockPancakePlatform.sol";
 
@@ -29,6 +30,7 @@ contract BnbGaugeVotingStateSenderTest is Utils {
     address internal constant USER_2_PROXY = 0xdf29565f309797e101a553471804073399242D71;
     address internal constant GAUGE = 0x6425bC30D0751aF5181fC74a50e760b0e4a19811;
     address internal constant GAUGE_2 = 0x9cac9745731d1Cf2B483f257745A512f0938DD01;
+    address internal constant GAUGE_VOTING = 0xf81953dC234cdEf1D6D0d3ef61b232C6bCbF9aeF;
     uint256 internal constant GAUGE_CHAIN_ID = 56;
     uint256 internal constant DST_CHAIN_ID = 42161;
 
@@ -63,12 +65,22 @@ contract BnbGaugeVotingStateSenderTest is Utils {
         assertGt(gaugeBias, 0);
         assertEq(claimData.length, 1);
 
+        uint256 currentPeriod = getCurrentPeriod();
+
+        bytes32 gaugeHash = keccak256(abi.encodePacked(GAUGE, GAUGE_CHAIN_ID));
+
+        uint256 lastVote = IGaugeVoting(GAUGE_VOTING).lastUserVote(USER, gaugeHash);
+
+        IGaugeVoting.VotedSlope memory userVotedSlope = IGaugeVoting(GAUGE_VOTING).voteUserSlopes(USER, gaugeHash);
+        uint256 userVoteBias = userVotedSlope.slope * (userVotedSlope.end - currentPeriod);
+
         // check user data
         assertEq(claimData[0].user, USER);
         assertGt(claimData[0].lastVote, 0);
-        assertGt(claimData[0].lastVote, getCurrentPeriod());
+        assertEq(claimData[0].lastVote, lastVote);
+        assertGt(claimData[0].lastVote, currentPeriod);
         assertGt(claimData[0].userVoteBias, 0);
-        //assertGt(claimData[0].userVoteEnd, getCurrentPeriod());
+        assertEq(claimData[0].userVoteBias, userVoteBias);
 
         (bool success,) = address(platform).call(payload);
         assertTrue(success);
@@ -93,25 +105,41 @@ contract BnbGaugeVotingStateSenderTest is Utils {
         assertGt(gaugeBias, 0);
         assertEq(claimData.length, 1);
 
+        bytes32 gaugeHash = keccak256(abi.encodePacked(GAUGE_2, GAUGE_CHAIN_ID));
+        uint256 userLastVote = IGaugeVoting(GAUGE_VOTING).lastUserVote(USER_2, gaugeHash);
+        uint256 proxyLastVote = IGaugeVoting(GAUGE_VOTING).lastUserVote(USER_2_PROXY, gaugeHash);
+
+        uint256 currentPeriod = getCurrentPeriod();
+
+        IGaugeVoting.VotedSlope memory userVotedSlope = IGaugeVoting(GAUGE_VOTING).voteUserSlopes(USER_2, gaugeHash);
+        uint256 userVoteBias = userVotedSlope.slope * (userVotedSlope.end - currentPeriod);
+
+        IGaugeVoting.VotedSlope memory proxyVotedSlope =
+            IGaugeVoting(GAUGE_VOTING).voteUserSlopes(USER_2_PROXY, gaugeHash);
+        uint256 proxyVoteBias = proxyVotedSlope.slope * (proxyVotedSlope.end - currentPeriod);
+
         // check user+proxy data
         assertEq(claimData[0].user, USER_2);
         assertGt(claimData[0].lastVote, 0);
-        assertGt(claimData[0].lastVote, getCurrentPeriod());
+        assertGt(claimData[0].lastVote, currentPeriod);
+        assertEq(userLastVote, proxyLastVote);
+        assertEq(claimData[0].lastVote, userLastVote);
         assertGt(claimData[0].userVoteBias, 0);
-        //assertGt(claimData[0].userVoteEnd, getCurrentPeriod());
-
-        // check proxy data
-        // assertEq(claimData[1].user, USER_2_PROXY);
-        // assertGt(claimData[1].lastVote, 0);
-        // assertGt(claimData[1].lastVote, getCurrentPeriod());
-        // assertGt(claimData[1].userVoteSlope, 0);
-        // assertEq(claimData[1].userVotePower, 10000);
-        // assertGt(claimData[1].userVoteEnd, getCurrentPeriod());
-
-        // assertEq(claimData[0].lastVote, claimData[1].lastVote);
+        assertEq(claimData[0].userVoteBias, userVoteBias + proxyVoteBias);
 
         (bool success,) = address(platform).call(payload);
         assertTrue(success);
+    }
+
+    function testSendClaimStateWithoutLockAndWithouProxy() external {
+        address user = address(0xABCD);
+
+        address[] memory blacklist;
+
+        uint256 minValue = sender.claimMinValue();
+
+        vm.expectRevert(BnbGaugeVotingStateSender.UserWithoutBias.selector);
+        sender.claimOnDstChain{value: minValue}(0, user, GAUGE_2, GAUGE_CHAIN_ID, DST_CHAIN_ID, blacklist);
     }
 
     function testSendClaimStateWithBlacklist() external {
@@ -133,6 +161,8 @@ contract BnbGaugeVotingStateSenderTest is Utils {
         // user+proxy
         // blacklist user 1
         assertEq(claimData.length, 2);
+
+        assertEq(claimData[1].user, USER);
     }
 
     function testSendClaimStateWithProxyInBlacklist() external {
@@ -152,10 +182,47 @@ contract BnbGaugeVotingStateSenderTest is Utils {
         (, IPlatform.ClaimData[] memory claimData) = this._encodePayload(payload);
 
         // user+proxy
-        // blacklist user 1
+        assertEq(claimData[0].user, USER_2);
+
+        // blacklist user 2 proxy
         assertEq(claimData.length, 2);
+        assertEq(claimData[1].user, USER_2_PROXY);
         assertEq(claimData[1].userVoteBias, 0);
         assertEq(claimData[1].lastVote, 0);
+    }
+
+    function testDisableVm() external {
+        // disable arbitrum as votemarket dst chain
+        sender.setVm(address(0), "arbitrum", DST_CHAIN_ID);
+        address[] memory blacklist;
+
+        vm.recordLogs();
+
+        sender.claimOnDstChain{value: sender.claimMinValue()}(
+            0, USER_2, GAUGE_2, GAUGE_CHAIN_ID, DST_CHAIN_ID, blacklist
+        );
+
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+
+        // no events emitted
+        assertEq(entries.length, 0);
+    }
+
+    function testSetRecipient() external {
+        address recipient = address(0xABCD);
+
+        vm.recordLogs();
+
+        sender.setRecipient{value: sender.setRecipientMinValue()}(DST_CHAIN_ID, recipient);
+
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+
+        (,, bytes memory payload) = abi.decode(entries[1].data, (string, string, bytes));
+
+        (address _user, address _recipient) = this._encodePayloadSetRecipient(payload);
+
+        assertEq(address(this), _user);
+        assertEq(recipient, _recipient);
     }
 
     function _encodePayload(bytes calldata _payload)
@@ -165,6 +232,14 @@ contract BnbGaugeVotingStateSenderTest is Utils {
     {
         (,,, gaugeBias, claimData) =
             abi.decode(_payload[4:], (uint256, address, uint256, uint256, IPlatform.ClaimData[]));
+    }
+
+    function _encodePayloadSetRecipient(bytes calldata _payload)
+        public
+        pure
+        returns (address user, address recipient)
+    {
+        (user, recipient) = abi.decode(_payload[4:], (address, address));
     }
 
     function getCurrentPeriod() public view returns (uint256) {
